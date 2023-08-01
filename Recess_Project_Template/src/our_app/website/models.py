ec49531eb0b06cfb13c2c23876572d2ece8dcae8
mkb2001 <kevinmugarura051@gmail.com>
2023-08-01 13:25:44 +0300

    Course

--- Recess_Project_Template/src/our_app/website/models.py
+++ Recess_Project_Template/src/our_app/website/models.py
@@ -1,15 +1,30 @@
-
 from django.db import models
+
 
 class InstructorFeedback(models.Model):
     instructorName = models.CharField(max_length=100)
     department = models.CharField(max_length=100)
     courseUnit = models.CharField(max_length=100)
-    knowledge = models.IntegerField(choices=[(20, 'Poor'), (40, 'Fair'), (60, 'Good'), (80, 'Very Good'), (100, 'Excellent')])
-    communication = models.IntegerField(choices=[(20, 'Poor'), (40, 'Fair'), (60, 'Good'), (80, 'Very Good'), (100, 'Excellent')])
-    teachingStyle = models.IntegerField(choices=[(20, 'Poor'), (40, 'Fair'), (60, 'Good'), (80, 'Very Good'), (100, 'Excellent')])
-    responsiveness = models.IntegerField(choices=[(20, 'Poor'), (40, 'Fair'), (60, 'Good'), (80, 'Very Good'), (100, 'Excellent')])
+    knowledge = models.IntegerField(
+        choices=[(20, 'Poor'), (40, 'Fair'), (60, 'Good'), (80, 'Very Good'), (100, 'Excellent')])
+    communication = models.IntegerField(
+        choices=[(20, 'Poor'), (40, 'Fair'), (60, 'Good'), (80, 'Very Good'), (100, 'Excellent')])
+    teachingStyle = models.IntegerField(
+        choices=[(20, 'Poor'), (40, 'Fair'), (60, 'Good'), (80, 'Very Good'), (100, 'Excellent')])
+    responsiveness = models.IntegerField(
+        choices=[(20, 'Poor'), (40, 'Fair'), (60, 'Good'), (80, 'Very Good'), (100, 'Excellent')])
     additional_comments = models.TextField()
 
     def __str__(self):
         return f"Feedback by {self.instructorName}"
+
+
+class Course(models.Model):
+    courseName = models.CharField(max_length=100)
+    courseCode = models.CharField(max_length=100)
+    courseDescription = models.CharField(max_length=1000)
+    effectiveness = models.IntegerField(
+        choices=[(20, 'Poor'), (40, 'Fair'), (60, 'Good'), (80, 'Very Good'), (100, 'Excellent')])
+    interest = models.IntegerField(
+        choices=[(20, 'Poor'), (40, 'Fair'), (60, 'Good'), (80, 'Very Good'), (100, 'Excellent')])
+    improvement = models.TextField()
--- Recess_Project_Template/src/our_app/website/views.py
+++ Recess_Project_Template/src/our_app/website/views.py
@@ -2,54 +2,71 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, CourseForm
 from .forms import InstructorForm
-from .models import InstructorFeedback
+from .models import InstructorFeedback, Course
 from django.shortcuts import render, redirect, get_object_or_404
 
 
-
 def home(request):
-	return render(request, 'index.html')
+    return render(request, 'index.html')
 
 
 def course(request):
-	return render(request, 'course.html')
+    return render(request, 'course.html')
+
+
+def course_feedback(request):
+    if request.method == 'POST':
+        form = CourseForm(request.POST)
+        if form.is_valid():
+            # Save the form data to the database
+            form_data = form.cleaned_data
+            feedback = Course(
+                course_name=form_data['courseName'],
+                course_description=form_data['courseDescription'],
+                courseCode=form_data['courseCode'],
+                effectiveness=form_data['effectiveness'],
+                interest=form_data['interest'],
+                improvement=form_data['improvement'],
+
+            )
+            feedback.save()
+
+            return redirect('home')  # Redirect to a success page after successful form submission
+    else:
+        form = CourseForm()
+
+    return render(request, 'course.html', {'courseForm': form})
 
 
 def facility(request):
-	return render(request, 'facility.html')
+    return render(request, 'facility.html')
 
 
 def instructor(request):
-	return render(request, 'instructor.html')
-
+    return render(request, 'instructor.html')
 
 
 def dashboard(request):
-	if request.user.is_authenticated:
-		return render(request, 'dashboard/pages/dashboard.html')
-	else:
-		messages.success(request, "You Must Be Logged In...")
-		return redirect('home')
-	
+    if request.user.is_authenticated:
+        return render(request, 'dashboard/pages/dashboard.html')
+    else:
+        messages.success(request, "You Must Be Logged In...")
+        return redirect('home')
 
 
 def courses(request):
-	return render(request, 'dashboard/pages/courses.html')
-
-
-
+    return render(request, 'dashboard/pages/courses.html')
 
 
 def facilities(request):
-	return render(request, 'dashboard/pages/facilities.html')
+    return render(request, 'dashboard/pages/facilities.html')
 
 
 def instructors(request):
     feedbacks = InstructorFeedback.objects.all()
     return render(request, 'dashboard/pages/instructors.html', {'feedbacks': feedbacks})
-
 
 
 from .models import InstructorFeedback
@@ -63,72 +80,58 @@ def delete_instructor_feedback(request, feedback_id):
     return render(request, 'dashboard/pages/delete_instructor_feedback.html', {'feedback': feedback})
 
 
-
-
 def profile(request):
-	if request.user.is_authenticated:
-		return render(request, 'dashboard/pages/profile.html')
-	else:
-		messages.success(request, "You Must Be Logged In...")
-		return redirect('sign_in')
-	
-	
-
+    if request.user.is_authenticated:
+        return render(request, 'dashboard/pages/profile.html')
+    else:
+        messages.success(request, "You Must Be Logged In...")
+        return redirect('sign_in')
 
 
 def signout(request):
-	logout(request)
-	messages.success(request, "You Have Been Logged Out...")
-	return redirect('home')
-
+    logout(request)
+    messages.success(request, "You Have Been Logged Out...")
+    return redirect('home')
 
 
 def signin(request):
-	if request.user.is_authenticated:
-		return render(request, 'dashboard/pages/dashboard.html')
-	else:
-		# Check to see if logging in
-		if request.method == 'POST':
-			username = request.POST['username']
-			password = request.POST['password']
-			# Authenticate
-			user = authenticate(request, username=username, password=password)
-			if user is not None:
-				login(request, user)
-				messages.success(request, "You Have Been Logged In!")
-				return redirect('dashboard')
-			else:
-				messages.success(request, "There Was An Error Logging In, Please Try Again...")
-				return redirect('home')
-		else:
-			return render(request, 'dashboard/pages/sign_in.html')
-		
-
-	
-
-
+    if request.user.is_authenticated:
+        return render(request, 'dashboard/pages/dashboard.html')
+    else:
+        # Check to see if logging in
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            # Authenticate
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "You Have Been Logged In!")
+                return redirect('dashboard')
+            else:
+                messages.success(request, "There Was An Error Logging In, Please Try Again...")
+                return redirect('home')
+        else:
+            return render(request, 'dashboard/pages/sign_in.html')
 
 
 def signup(request):
-	if request.method == 'POST':
-		form = SignUpForm(request.POST)
-		if form.is_valid():
-			form.save()
-			# Authenticate and login
-			username = form.cleaned_data['username']
-			password = form.cleaned_data['password1']
-			user = authenticate(username=username, password=password)
-			login(request, user)
-			messages.success(request, "You Have Successfully Registered! Welcome!")
-			return redirect('dashboard')
-	else:
-		form = SignUpForm()
-		return render(request, 'dashboard/pages/sign_up.html', {'form':form})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered! Welcome!")
+            return redirect('dashboard')
+    else:
+        form = SignUpForm()
+        return render(request, 'dashboard/pages/sign_up.html', {'form': form})
 
-	return render(request, 'dashboard/pages/sign_up.html', {'form':form})
-
-
-
+    return render(request, 'dashboard/pages/sign_up.html', {'form': form})
 
 
 def instructor_feedback(request):
@@ -154,4 +157,3 @@ def instructor_feedback(request):
         form = InstructorForm()
 
     return render(request, 'instructor.html', {'form': form})
-
