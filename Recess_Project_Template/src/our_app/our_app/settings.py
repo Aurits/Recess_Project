d3741c7be867f8ff1384fb9d2dd980bb56063378
Aurits <87270662+Aurits@users.noreply.github.com>
2023-08-02 11:34:55 +0300

    project setup

--- Recess_Project_Template/src/our_app/our_app/settings.py
+++ Recess_Project_Template/src/our_app/our_app/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'recess_project',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'Aurit',
         'HOST': 'localhost',
         'PORT': '3306',
     }
